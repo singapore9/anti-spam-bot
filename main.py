75f6726cb30e28b21a919ecdb6e68dba677d800d
singapore9 <sharik.and@gmail.com>
2022-10-03 20:14:17 +0300

    Allow ban persons by specifed patterns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import logging
+import re
 
 from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, CallbackContext, MessageHandler)
-from telegram.ext.filters import TEXT, COMMAND
+from telegram.ext.filters import TEXT, COMMAND, ALL
 
-from constants import TELEGRAM_TOKEN
+from constants import TELEGRAM_TOKEN, BLOCK_USERS_WITH_NAMES_REGEXP
 
 
 logging.basicConfig(
@@ -17,13 +18,24 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def echo(update: Update, context: CallbackContext) -> None:
-    await update.effective_message.reply_text(update.effective_message.text)
+async def check_and_ban_new_members(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_message.chat_id
+    new_chat_members = update.effective_message.new_chat_members
+    for new_chat_member in new_chat_members:
+        member_id = new_chat_member.id
+        full_name = new_chat_member.full_name
+        try:
+            for pattern in BLOCK_USERS_WITH_NAMES_REGEXP:
+                if re.fullmatch(pattern, full_name):
+                    await context.bot.banChatMember(chat_id=chat_id, user_id=member_id)
+                    break
+        except Exception as e:
+            await update.effective_message.reply_text(f'failed: {e}')
 
 
 def get_tg_application():
     tg_application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    tg_application.add_handler(MessageHandler(TEXT & ~COMMAND, echo))
+    tg_application.add_handler(MessageHandler(filters=ALL, callback=check_and_ban_new_members))
     return tg_application
 
 
